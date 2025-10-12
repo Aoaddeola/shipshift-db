ab3bfb63fe39b12dd88d907fb1135049e77c74b1
aoaddeola <pfhemmy@protonmail.com>
2025-10-12 19:43:03 +0100

    Convert operator address to raw during web3 auth

--- src/auth/operator/auth.controller.ts
+++ src/auth/operator/auth.controller.ts
@@ -10,6 +10,8 @@ import {
 import { AuthService } from './auth.service.js';
 import { DataSignature } from '@meshsdk/common';
 import { OperatorService } from '../../users/operator/operator.service.js';
+import 'bech32';
+import { bech32 } from 'bech32';
 
 @Controller('web3/auth')
 export class AuthController {
@@ -33,10 +35,33 @@ export class AuthController {
     if (!isValid) {
       throw new HttpException('Invalid signature', HttpStatus.UNAUTHORIZED);
     }
-    const operator = await this.operatorService.getOperatorByAddress(address);
+    const operator = await this.operatorService.getOperatorByAddress(
+      convertAddrToRaw(address),
+    );
     return {
       accessToken: await this.authService.generateToken(operator),
       user: { ...operator, userType: 'operator' },
     };
   }
 }
+
+export const convertAddrToRaw = (addr: string) => {
+  let converted: string = '';
+  const isHexadecimal = (str: string): boolean => {
+    const hexRegExp = /^[0-9a-fA-F]+$/;
+    return hexRegExp.test(str);
+  };
+  try {
+    converted = Buffer.from(
+      bech32.fromWords(bech32.decode(addr, 1000).words),
+    ).toString('hex');
+  } catch (error) {
+    if (isHexadecimal(addr) && addr.length == 128) {
+      console.log('Address already converted to raw', error);
+      converted = addr;
+    } else {
+      console.log('Error converting address to raw', error);
+    }
+  }
+  return converted;
+};
