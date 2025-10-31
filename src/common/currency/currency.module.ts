966f161bfcc084ef6cb2d1c99f2ed311f674bcb9
aoaddeola <pfhemmy@protonmail.com>
2025-10-31 13:16:07 +0100

    Add missing module in currency

--- src/common/currency/currency.module.ts
+++ src/common/currency/currency.module.ts
@@ -3,12 +3,14 @@ import { CurrencyController } from './currency.controller.js';
 import { CurrencyService } from './currency.service.js';
 import { IPFSAccessController } from '@orbitdb/core';
 import { OrbitDBModule } from '../../db/orbitdb/orbitdb.module.js';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     OrbitDBModule.forDatabase('currency', {
       AccessController: IPFSAccessController({ write: ['*'] }),
     }),
+    JwtModule,
   ],
   controllers: [CurrencyController],
   providers: [CurrencyService],
