77efcba9a33a1e18ab13d323268baa1c8193b540
aoaddeola <pfhemmy@protonmail.com>
2025-11-27 13:02:00 +0100

    Add delivery operator auth guard

---
+++ src/guards/jwt-deliveryOp-auth.guard.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+// src/guards/jwt-auth.guard.ts
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { AppConfigService } from '../config/config.service.js';
+import { UserType } from '../users/user/user.types.js';
+
+@Injectable()
+export class JwtDeliveryOpAuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: AppConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.jwtAccessSecret,
+      });
+      request.user = payload;
+      if (payload.userType !== UserType.OPERATOR) {
+        throw new UnauthorizedException('Not a delivery operator');
+      }
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
