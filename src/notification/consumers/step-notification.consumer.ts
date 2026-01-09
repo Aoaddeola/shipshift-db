40cab1c2fa16d3569baffcc08b1388b60d937c65
Fiftyw3bs <56092068+Fiftyw3bs@users.noreply.github.com>
2026-01-09 14:33:14 -0800

    Remove priority from  consumer (#17)
    Co-authored-by: aoaddeola <pfhemmy@protonmail.com>

--- src/notification/consumers/step-notification.consumer.ts
+++ src/notification/consumers/step-notification.consumer.ts
@@ -79,57 +79,57 @@ export class StepNotificationConsumer {
     }
   }
 
-  // Listen for system events that should trigger notifications
-  @RabbitSubscribe({
-    exchange: RabbitMQConfig.EXCHANGES.APP_EVENTS,
-    routingKey: RabbitMQConfig.STEP.EVENTS.STATE_CHANGED,
-    queue: 'notifications.system.events.queue',
-    queueOptions: RabbitMQConfig.QUEUE_OPTIONS.WITH_DLQ,
-  })
-  async handleStateChange(_message: any) {
-    const message = _message.data;
-    this.logger.debug(
-      `Processing notifications event for step: ${message.stepId}`,
-    );
+  // // Listen for system events that should trigger notifications
+  // @RabbitSubscribe({
+  //   exchange: RabbitMQConfig.EXCHANGES.APP_EVENTS,
+  //   routingKey: RabbitMQConfig.STEP.EVENTS.STATE_CHANGED,
+  //   queue: 'notifications.system.events.queue',
+  //   queueOptions: RabbitMQConfig.QUEUE_OPTIONS.WITH_DLQ,
+  // })
+  // async handleStateChange(_message: any) {
+  //   const message = _message.data;
+  //   this.logger.debug(
+  //     `Processing notifications event for step: ${message.stepId}`,
+  //   );
 
-    const contactDetails = (
-      await this.contactDetailsService.findByOwner(message.assigneeId)
-    )[0];
+  //   const contactDetails = (
+  //     await this.contactDetailsService.findByOwner(message.assigneeId)
+  //   )[0];
 
-    const agent = await this.userService.findById(message.assigneeId);
+  //   const agent = await this.userService.findById(message.assigneeId);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { createdAt, updatedAt, ...cDetails } = contactDetails.dataValues;
-    const notification: SingleNotificationEntityDto = {
-      userId: message.assigneeId,
-      recipientMap: createRecipientMapConcise(cDetails),
-      urgency: 'low',
-      userPreferences: cDetails.preference,
-      isUserOnline: false,
-      event: 'step.assigned',
-      userName: agent!.dataValues.name || 'User',
-      locale: 'en',
-      status: NotificationStatus.PENDING,
-      retryCount: 0,
-      channelsToUse: [],
-      variables: {
-        stepId: message.stepId,
-        shipmentId: message.shipmentId,
-        journeyId: message.journeyId,
-        agentId: message.agentId,
-        operatorId: message.operatorId,
-        dueDate: '100000',
-      },
-    };
+  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //   const { createdAt, updatedAt, ...cDetails } = contactDetails.dataValues;
+  //   const notification: SingleNotificationEntityDto = {
+  //     userId: message.assigneeId,
+  //     recipientMap: createRecipientMapConcise(cDetails),
+  //     urgency: 'low',
+  //     userPreferences: cDetails.preference,
+  //     isUserOnline: false,
+  //     event: 'step.assigned',
+  //     userName: agent!.dataValues.name || 'User',
+  //     locale: 'en',
+  //     status: NotificationStatus.PENDING,
+  //     retryCount: 0,
+  //     channelsToUse: [],
+  //     variables: {
+  //       stepId: message.stepId,
+  //       shipmentId: message.shipmentId,
+  //       journeyId: message.journeyId,
+  //       agentId: message.agentId,
+  //       operatorId: message.operatorId,
+  //       dueDate: '100000',
+  //     },
+  //   };
 
-    try {
-      await this.notificationService.processSingleNotification(notification);
-    } catch (error) {
-      this.logger.error(
-        `Failed to process system event ${message.type}:`,
-        error,
-      );
-      throw error;
-    }
-  }
+  //   try {
+  //     await this.notificationService.processSingleNotification(notification);
+  //   } catch (error) {
+  //     this.logger.error(
+  //       `Failed to process system event ${message.type}:`,
+  //       error,
+  //     );
+  //     throw error;
+  //   }
+  // }
 }
